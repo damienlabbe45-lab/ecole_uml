10354fecacbe6a9b127aefc5774d5c048156ffa8
Anaïs BINET <anais.binet@fms-ea.com>
2026-08-26 13:24:01 +0200

    Implémentation méthode : create_teacher. Création méthodes : del_teacher et create student

--- Code/src/school/Director.java
+++ Code/src/school/Director.java
@@ -1,5 +1,29 @@
 package school;
 
-public class Director {
+import java.util.ArrayList;
+
+public class Director extends Users {
+
+	public Director(int id, String name, String surname) {
+		super(id, name, surname);
+	}
+
+	@Override
+	public void show_courses(ArrayList all_courses) {
+		
+	}
+	
+	public Teacher create_teacher(Users user, Adresse adresse) {
+		Teacher teacher = new Teacher(user.getId(), user.getName(), user.getSurname(), adresse);
+		return teacher;
+	}
+	
+	public void del_teacher(ArrayList<Teacher> all_teacher) {
+		
+	}
+	
+	public Student create_student(Users users, Adresse adresse) {
+		
+	}
 
 }
