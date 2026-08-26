01132abd94009f4530f7f2ecf874c1a8c4e2ae5b
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-26 15:13:32 +0200

    ajout du constructeur , des getters et des setters. le setter des localDate est volontairement pas mis pour le moment

--- Code/src/school/Courses.java
+++ Code/src/school/Courses.java
@@ -1,5 +1,43 @@
 package school;
+import java.time.LocalDate;
+
 
 public class Courses {
+    String nameCourse;
+    Teacher teacherCourses;
+    LocalDate dateBegin;
+    LocalDate dateEnd;
+
+    public Courses(String nameCourse, Teacher teacherCourses, LocalDate dateBegin, LocalDate dateEnd) {
+        setNameCourse(nameCourse);
+        setTeacherCourses(teacherCourses);
+        this.dateBegin = dateBegin;
+        this.dateEnd = dateEnd;
+    }
+
+    public void setNameCourse(String nameCourse) {
+        this.nameCourse = nameCourse;
+    }
+
+    public void setTeacherCourses(Teacher teacherCourses) {
+        this.teacherCourses = teacherCourses;
+    }
+
+    
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+
+    public Teacher getTeacherCourses() {
+        return teacherCourses;
+    }
+    
+    public LocalDate getDateBegin() {
+        return dateBegin;
+    }
+    public LocalDate getDateEnd() {
+        return dateEnd;
+    }
 
 }
