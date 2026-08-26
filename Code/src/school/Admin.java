decebc673ac87304b311ca5c8c5c669c7be04f67
Anaïs BINET <anais.binet@fms-ea.com>
2026-08-26 13:22:25 +0200

    Correction type arraylist, implémentation méthode : create_director

--- Code/src/school/Admin.java
+++ Code/src/school/Admin.java
@@ -1,5 +1,23 @@
 package school;
 
-public class Admin {
+import java.util.ArrayList;
+
+public class Admin extends Users {
+
+	public Admin(int id, String name, String surname) {
+		super(id, name, surname);
+	}
+
+	@Override
+	public void show_courses(ArrayList<Courses> all_courses) {
+		for (Courses courses : all_courses) {
+			System.out.println(courses);
+		}
+	}
+	
+	public Director create_director(Users user) {
+		Director director = new Director(user.getId(), user.getName(), user.getSurname());
+		return director;
+	}
 	
 }
