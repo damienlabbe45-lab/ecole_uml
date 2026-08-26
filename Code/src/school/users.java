270b7ba3dd73d266b35bf6007bc149f709991cb3
Anaïs BINET <anais.binet@fms-ea.com>
2026-08-26 10:56:23 +0200

    Implémentation de la classe users

--- Code/src/school/users.java
+++ Code/src/school/users.java
@@ -1,5 +1,31 @@
 package school;
 
-public class users {
+import java.util.ArrayList;
+
+public abstract class users {
+	private int id;
+	private String name;
+	private String surname;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public abstract void show_courses(ArrayList all_courses);
 
 }
