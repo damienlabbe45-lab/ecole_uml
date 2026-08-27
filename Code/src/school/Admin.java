49c378ae560aaffbbf5b54cd209376608cdc3d85
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-27 15:34:46 +0200

    ajout de la javadoc

--- Code/src/school/Admin.java
+++ Code/src/school/Admin.java
@@ -2,19 +2,44 @@ package school;
 
 import java.util.ArrayList;
 
+/**
+* Représente l'admin de l'application et de la base de donnée
+* 
+* @version 1.0
+*/
 public class Admin extends Users {
 
+    /**
+    * Crée un nouveau admin
+    *
+    * @param id     l'identifiant de l'admin
+    * @param teacherCourses le prénom de l'admin
+    * @param dateBegin      le nom de l'admin
+    */
 	public Admin(int id, String name, String surname) {
 		super(id, name, surname);
 	}
 
+	/**
+    * affiche la liste des courses
+    *
+    * @param all_courses     la liste de tout les cours
+    */
 	@Override
 	public void show_courses(ArrayList<Courses> all_courses) {
 		for (Courses courses : all_courses) {
 			System.out.println(courses);
 		}
 	}
-	
+
+	/**
+    * Crée un nouveau admin
+    *
+    * @param id     l'identifiant du directeur
+    * @param teacherCourses le prénom du directeur
+    * @param dateBegin      le nom du directeur
+	* @return Director (retourne le directeur)
+    */
 	public Director create_director(Users user) {
 		return new Director(user.getId(), user.getName(), user.getSurname());
 	}
