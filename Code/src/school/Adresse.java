3015f1a1d025e50585c677c6ed7d199e1f4ce404
Alban MARTEL <albanmartel@users.noreply.github.com>
2026-08-27 09:48:48 +0200

    Update Adresse.java ajout d'une méthode init pour ajouter les informations de distances

--- Code/src/school/Adresse.java
+++ Code/src/school/Adresse.java
@@ -19,6 +19,7 @@ public class Adresse {
 	 */
 	public Adresse(String street, String city, int codePostal) {
 		distances.put("toulouse", 0);
+		init();
 		this.street = street;
 		this.city = city;
 		this.codePostal = codePostal;
@@ -30,19 +31,65 @@ public class Adresse {
 	 */
 	public Adresse(String city) {
 		distances.put("toulouse", 0);
+		init();
 		this.city = city;
 	}
 
 	public Adresse(String city, int codePostal) {
 		distances.put("toulouse", 0);
+		init();
 		this.city = city;
 		this.codePostal = codePostal;
 	}
 	
 	public Adresse(String street, String city) {
 		distances.put("toulouse", 0);
+		init();
 		this.street = street;
 		this.city = city;
+	}
+
+	/**
+	 * Cette fonction d'apporter les informations de distances
+	 * 
+	 * Hypothèse prise l'école est a Toulouse
+	 *
+	 * Les distances entre toulouse et la ville en question
+	 * tableau associatif clef: String, valeur: Int
+	 * clef, la ville de l'élève
+	 * distance, un int
+	 */
+	private void init () {
+	    distances.put("blagnac", 7);
+	    distances.put("balma", 7);
+	    distances.put("colomiers", 12);
+	    distances.put("ramonville-saint-agne", 12);
+	    distances.put("tournefeuille", 13);
+	    distances.put("union", 12);
+	    distances.put("aucamville", 12);
+	    distances.put("saint-orens-de-gameville", 15);
+	    distances.put("cugnaux", 16);
+	    distances.put("fonsorbes", 25);
+	    distances.put("leguevin", 25);
+	    distances.put("grenade", 30);
+	    distances.put("fronton", 30);
+	    distances.put("muret", 25);
+	    distances.put("auterive", 35);
+	    distances.put("villefranche-de-lauragais", 35);
+	    distances.put("carbonne", 45);
+	    distances.put("revel", 50);
+
+	    // Villes situées autour de Toulouse
+	    distances.put("montauban", 55);
+	    distances.put("albi", 75);
+	    distances.put("castres", 80);
+	    distances.put("carcassonne", 95);
+	    distances.put("auch", 80);
+	    distances.put("tarbes", 155);
+	    distances.put("foix", 90);
+	    distances.put("saint-gaudens", 95);
+	    distances.put("cahors", 115);
+	    distances.put("pamiers", 75);
 	}
 
 }
