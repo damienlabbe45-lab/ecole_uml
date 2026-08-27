70ad80c9afae91d7038ac2860189367bc6d715b0
Alban MARTEL <albanmartel@users.noreply.github.com>
2026-08-27 09:45:09 +0200

    Update Adresse.java ajout des paramètres et du constructeur de la classe

--- Code/src/school/Adresse.java
+++ Code/src/school/Adresse.java
@@ -1,5 +1,27 @@
 package school;
 
+import java.util.HashMap;
+
 public class Adresse {
+  	/**
+	 * Hypothèse la ville de l'école est Toulouse
+	 */
+	private HashMap<String, Integer> distances = new HashMap<String, Integer>();
+	private String street = "unknown";
+	private String city;
+	private int codePostal = 0;
+
+  	/**
+	 * Constructeur de la classe avec l'ensemble des paramètres définis
+	 * @param street
+	 * @param city
+	 * @param codePostal Entier 
+	 */
+	public Adresse(String street, String city, int codePostal) {
+		distances.put("toulouse", 0);
+		this.street = street;
+		this.city = city;
+		this.codePostal = codePostal;
+	}
 
 }
