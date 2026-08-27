60c774b674a8ced315399bdedb47fe4aa4218e09
Alban MARTEL <alban.martel@fms-ea.com>
2026-08-27 13:32:49 +0200

    Update des tests

--- Code/src/school/test.java
+++ Code/src/school/test.java
@@ -1,12 +1,30 @@
 package school;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class test {
 	public static void main(String[] args) {
 		// Test de la classe Admin
         Admin admin = new Admin(2, "Alice", "Dupont");
         verifier("Admin hérite de User", admin instanceof Users);
+        Director directeur = new Director(1, "Robert", "Dupont");        
+        Teacher jdoeTeacher = directeur.create_teacher(0, "John", "Doe", new Adresse("Auch"));
+        System.out.println("Affichage d'un professeur"); 
+        Courses cours = directeur.createCours("Algorithmique", LocalDate.of(2026, 6, 22), LocalDate.of(2026, 8, 12), jdoeTeacher);
+        ArrayList<Courses>listeCours = new ArrayList<Courses>();
+        listeCours.add(cours);
+        directeur.show_courses(listeCours);
+        System.out.println(jdoeTeacher);
+        Adresse testAdresse1 = new Adresse("Toulouse");
+        System.out.println(testAdresse1.toString());        
 	}
 	
+	/**
+	 * Test si une instance hérite d'une classe mère
+	 * @param nomDuTest
+	 * @param condition
+	 */
 	private static void verifier(String nomDuTest, boolean condition) {
         if (condition) {
             System.out.println("[OK] " + nomDuTest);
@@ -14,4 +32,5 @@ public class test {
             System.out.println("[ÉCHEC] " + nomDuTest);
         }
     }
+	
 }
