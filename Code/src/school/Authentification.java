ffc391ea19af1b6de9b03a89989f69502066897b
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-27 16:01:18 +0200

    modification de authentification pour que ca correspond

--- Code/src/school/Authentification.java
+++ Code/src/school/Authentification.java
@@ -2,10 +2,6 @@ package school;
 
 import java.util.HashMap;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 537bcce77b827d79254ec4b3db1993cd06313cb8
 public class Authentification {
 	HashMap <String, String> loginPassword = new HashMap<String, String>();
 	private String login = "Any";
@@ -57,9 +53,7 @@ public class Authentification {
 	}
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		
-=======
+
 		Authentification test = new Authentification();
 		test.addLogin("toto", "titi");
 		String messageString = "Authentification a échouée\nMauvais identifiant ou mauvais mot de passe !";
@@ -67,7 +61,7 @@ public class Authentification {
 			messageString = "Authentification réussie !";
 		}
 		System.out.println(messageString);	
->>>>>>> 537bcce77b827d79254ec4b3db1993cd06313cb8
+
 	}	
 
 }
